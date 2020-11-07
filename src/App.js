98367201dda87a3acf883ee8a3683ccf08555022
Enrique Hernandez <enrique.hz.t@gmail.com>
2020-11-07 14:44:39 -0600

    Ready to test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import CardItem from "./components/CardItem";
 import { pokemons } from "./mock/mockedData";
-
 import "./styles.css";
 
 export default function App() {
-  /**
-   * Input state management logic here
-   */
-
+  const [name, setName] = useState("");
+  // console.log(pokemons);
+  let pokeList = [];
+  //const result = words.filter(word => word.length > 6);
+  const matchPokemon = pokemons.filter((pokemon) => pokemon.name === name);
+  matchPokemon.forEach((item) => {
+    pokeList.push(<CardItem name={item.name} sprite={item.sprite} />);
+  });
+  //console.log(name)
+  //console.log(matchPokemon);
+  //console.log(pokeList);
   return (
     <div className="app">
       <h2>Mini Challenge 3: Poke filter</h2>
-      {/**
-       Insert here the input tag
-       **/}
-      <div className="cards">
-        {/**
-        Filter pokemons data and map them to return an array of CardItems.
-         */}
-      </div>
+      {/*<h2>Preview:{name}</h2>*/}
+      <input
+        name="name"
+        type="text"
+        placeholder="Enter the Pokémon name"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+
+      {/*}
+     <button> Insert Pokemon</button>
+      */}
+
+      <div className="cards">{pokeList}</div>
     </div>
   );
 }
